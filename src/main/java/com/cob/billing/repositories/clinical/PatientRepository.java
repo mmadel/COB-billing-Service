a9b8ea0398f75a38de6692b1951a90b8be96e54c
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-12-16 18:14:27 +0200

    enhance findBySessionSubmittedByPatient

--- src/main/java/com/cob/billing/repositories/clinical/PatientRepository.java
+++ src/main/java/com/cob/billing/repositories/clinical/PatientRepository.java
@@ -2,6 +2,7 @@ package com.cob.billing.repositories.clinical;
 
 import com.cob.billing.entity.clinical.patient.PatientEntity;
 import com.cob.billing.entity.clinical.patient.session.PatientSessionEntity;
+import com.cob.billing.entity.clinical.patient.session.ServiceLineEntity;
 import com.cob.billing.enums.PatientSessionStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,13 +11,14 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Objects;
 
 public interface PatientRepository extends PagingAndSortingRepository<PatientEntity, Long> {
     @Query("SELECT distinct pe FROM PatientEntity pe  JOIN pe.sessions session WHERE (session.status = 'Prepare' OR session.status = 'Partial')")
     Page<PatientEntity> findBySessionNotSubmittedByPatient(Pageable paging);
 
-    @Query("SELECT distinct pe FROM PatientEntity pe  JOIN pe.sessions session WHERE session.status = 'Submit' AND pe.id =:patientId")
-    Page<PatientEntity> findBySessionSubmittedByPatient(Pageable paging, @Param("patientId") Long patientId);
+    @Query("SELECT serviceCode  , session FROM PatientEntity pe  JOIN pe.sessions session JOIN session.serviceCodes serviceCode WHERE session.status = 'Submit' AND pe.id =:patientId")
+    Page<Object> findBySessionSubmittedByPatient(Pageable paging, @Param("patientId") Long patientId);
 
     @Query("SELECT distinct pe FROM PatientEntity pe  JOIN pe.sessions session WHERE (session.status = 'Prepare' OR session.status = 'Partial') AND pe.id =:patientId")
     PatientEntity findBySessionStatusByPatient(@Param("patientId") Long patientId);
--- src/main/java/com/cob/billing/usecases/bill/invoice/FindSubmittedPatientSessionUseCase.java
+++ src/main/java/com/cob/billing/usecases/bill/invoice/FindSubmittedPatientSessionUseCase.java
@@ -1,10 +1,12 @@
 package com.cob.billing.usecases.bill.invoice;
 
 import com.cob.billing.entity.clinical.patient.PatientEntity;
-import com.cob.billing.model.bill.posting.ClientPostingPayments;
+import com.cob.billing.entity.clinical.patient.session.PatientSessionEntity;
+import com.cob.billing.entity.clinical.patient.session.ServiceLineEntity;
 import com.cob.billing.model.bill.posting.PaymentServiceLine;
 import com.cob.billing.model.clinical.patient.Patient;
 import com.cob.billing.model.clinical.patient.session.PatientSession;
+import com.cob.billing.model.clinical.patient.session.ServiceLine;
 import com.cob.billing.model.response.ClientPostingPaymentResponse;
 import com.cob.billing.repositories.clinical.PatientRepository;
 import org.modelmapper.ModelMapper;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
@@ -26,45 +29,35 @@ public class FindSubmittedPatientSessionUseCase {
     ModelMapper mapper;
 
     public ClientPostingPaymentResponse findClient(Pageable paging, Long clientId) {
-        Page<PatientEntity> pages = patientRepository.findBySessionSubmittedByPatient(paging, clientId);
+        Page<Object> pages = patientRepository.findBySessionSubmittedByPatient(paging, clientId);
         long total = (pages).getTotalElements();
-        List<ClientPostingPayments> records = pages.stream()
-                .map(patientEntity -> {
-                    ClientPostingPayments clientPostingPayments = new ClientPostingPayments();
-                    Patient patient = mapper.map(patientEntity, Patient.class);
-                    clientPostingPayments.setClientId(patient.getId());
-                    clientPostingPayments.setPaymentServiceLines(constructServiceLines(patient.getSessions()));
-
-                    return clientPostingPayments;
-                })
-                .collect(Collectors.toList());
+        List<PaymentServiceLine> records = new ArrayList<>();
+        pages.getContent().stream().forEach(o -> {
+            Object[] result = (Object[]) o;
+            ServiceLineEntity serviceLine = (ServiceLineEntity) result[0];
+            PatientSessionEntity session = (PatientSessionEntity) result[1];
+            records.add(constructServiceLine(serviceLine, session));
+        });
 
         return ClientPostingPaymentResponse.builder()
                 .number_of_records(records.size())
                 .number_of_matching_records((int) total)
                 .records(records)
                 .build();
-
     }
 
     public void findInsuranceCompany(Long insuranceCompanyId) {
 
     }
 
-    private List<PaymentServiceLine> constructServiceLines(List<PatientSession> sessions) {
-        List<PaymentServiceLine> paymentServiceLines = new ArrayList<>();
-        sessions.forEach(patientSession -> {
-            patientSession.getServiceCodes().forEach(serviceLine -> {
-                paymentServiceLines.add(PaymentServiceLine.builder()
-                        .sessionId(patientSession.getId())
-                        .ServiceCodeId(serviceLine.getId())
-                        .dateOfService(patientSession.getServiceDate())
-                        .cpt(serviceLine.getCptCode().getServiceCode() + "." + serviceLine.getCptCode().getModifier())
-                        .provider(patientSession.getDoctorInfo().getDoctorLastName() + "," + patientSession.getDoctorInfo().getDoctorFirstName())
-                        .billedValue(serviceLine.getCptCode().getCharge())
-                        .build());
-            });
-        });
-        return paymentServiceLines;
+    private PaymentServiceLine constructServiceLine(ServiceLineEntity serviceLine, PatientSessionEntity session) {
+        return PaymentServiceLine.builder()
+                .sessionId(session.getId())
+                .ServiceCodeId(serviceLine.getId())
+                .dateOfService(session.getServiceDate())
+                .cpt(serviceLine.getCptCode().getServiceCode() + "." + serviceLine.getCptCode().getModifier())
+                .billedValue(serviceLine.getCptCode().getCharge())
+                .provider(session.getDoctorInfo().getDoctorLastName() + "," + session.getDoctorInfo().getDoctorFirstName())
+                .build();
     }
 }
