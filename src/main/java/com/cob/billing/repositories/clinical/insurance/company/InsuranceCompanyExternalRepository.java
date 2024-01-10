69b02496ce2942b7b057b9d8e1287c8bb354f77f
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-10 16:43:15 +0200

    add new Repositories

---
+++ src/main/java/com/cob/billing/repositories/clinical/insurance/company/InsuranceCompanyExternalRepository.java
@@ -0,0 +1,7 @@
+package com.cob.billing.repositories.clinical.insurance.company;
+
+import com.cob.billing.entity.clinical.insurance.compnay.InsuranceCompanyExternalEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface InsuranceCompanyExternalRepository extends CrudRepository<InsuranceCompanyExternalEntity,Long> {
+}
---
+++ src/main/java/com/cob/billing/repositories/clinical/insurance/company/PatientInsuranceInternalCompanyRepository.java
@@ -0,0 +1,7 @@
+package com.cob.billing.repositories.clinical.insurance.company;
+
+import com.cob.billing.entity.clinical.insurance.compnay.PatientInsuranceInternalCompanyEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientInsuranceInternalCompanyRepository extends CrudRepository<PatientInsuranceInternalCompanyEntity,Long> {
+}
