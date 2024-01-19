048f0308093402bb90fff7d31aa51743264b6b25
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-19 17:25:31 +0200

    expose end point to add payer for simulator

--- src/main/java/com/cob/billing/controller/bill/PayerController.java
+++ src/main/java/com/cob/billing/controller/bill/PayerController.java
@@ -1,20 +1,28 @@
 package com.cob.billing.controller.bill;
 
+import com.cob.billing.model.bill.payer.Payer;
+import com.cob.billing.usecases.bill.payer.AddPayerUseCase;
 import com.cob.billing.usecases.bill.payer.FindPayerUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/payer")
 public class PayerController {
     @Autowired
     FindPayerUseCase findPayerUseCase;
+    @Autowired
+    AddPayerUseCase addPayerUseCase;
+
     @GetMapping("/find")
     public ResponseEntity findAll() {
         return new ResponseEntity(findPayerUseCase.find(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity create(@RequestBody Payer model) {
+        return new ResponseEntity(addPayerUseCase.add(model), HttpStatus.OK);
     }
 }
