552e1974fff01ea862bb33c59016b160e7ee65f3
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-01-30 13:48:24 +0200

    map returned value from  find provider by npi from nppes to be provider model

--- src/main/java/com/cob/billing/usecases/integration/nppes/FindProviderByNPIUseCase.java
+++ src/main/java/com/cob/billing/usecases/integration/nppes/FindProviderByNPIUseCase.java
@@ -1,6 +1,10 @@
 package com.cob.billing.usecases.integration.nppes;
 
+import com.cob.billing.model.clinical.provider.Provider;
+import com.cob.billing.model.clinical.provider.ProviderInfo;
+import com.cob.billing.model.common.Address;
 import com.cob.billing.model.integration.nppes.NPPESResponse;
+import com.cob.billing.model.integration.nppes.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -15,9 +19,31 @@ public class FindProviderByNPIUseCase {
     @Value("${nppes.base.url}")
     private String nppesBaseUrl;
 
-    public NPPESResponse find(Long npi) {
+    public Provider find(Long npi) {
         String url = nppesBaseUrl + "?version=2.1&number=" + npi;
         NPPESResponse result = restTemplate.getForObject(url, NPPESResponse.class);
-        return result;
+        return map(result);
+    }
+
+    private Provider map(NPPESResponse nppesResponse) {
+        Result result = nppesResponse.getResults().get(0);
+        Provider provider = new Provider();
+        provider.setFirstName(result.getBasic().getFirst_name());
+        provider.setLastName(result.getBasic().last_name);
+        provider.setPhone(result.getAddresses().get(0).getTelephone_number());
+        provider.setNpi(result.number);
+        Address address = new Address();
+        address.setFirst(result.getAddresses().get(0).address_1);
+        address.setSecond(result.getAddresses().get(0).address_2);
+        address.setCountry(result.getAddresses().get(0).country_name);
+        address.setCity(result.getAddresses().get(0).city);
+        address.setState(result.getAddresses().get(0).state);
+        address.setZipCode(result.getAddresses().get(0).postal_code);
+        provider.setAddress(address);
+        ProviderInfo providerInfo = new ProviderInfo();
+        providerInfo.setTaxonomyCode(result.getTaxonomies().get(0).code);
+        providerInfo.setLicense(result.getTaxonomies().get(0).license);
+        provider.setProviderInfo(providerInfo);
+        return provider;
     }
 }
