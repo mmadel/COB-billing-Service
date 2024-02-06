4fa1662cef8604176eb329052853d6b8f6ce292d
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-02-06 22:15:11 +0200

    add ClaimUploadRequest fields

--- src/main/java/com/cob/billing/model/integration/claimmd/ClaimUploadRequest.java
+++ src/main/java/com/cob/billing/model/integration/claimmd/ClaimUploadRequest.java
@@ -3,7 +3,11 @@ package com.cob.billing.model.integration.claimmd;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 @Getter
 public class ClaimUploadRequest {
+    public String fileid;
+    public List<Claim> claim;
 }
