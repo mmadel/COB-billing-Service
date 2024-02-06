c7133b6039110329b3d6e4ee08e1aad1cf091966
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-02-06 22:11:12 +0200

    add charge fields

--- src/main/java/com/cob/billing/model/integration/claimmd/Charge.java
+++ src/main/java/com/cob/billing/model/integration/claimmd/Charge.java
@@ -6,4 +6,14 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Charge {
+    public String charge;
+    public String charge_record_type;
+    public String diag_ref;
+    public String from_date;
+    public String place_of_service;
+    public String proc_code;
+    public String remote_chgid;
+    public String thru_date;
+    public String units;
+    public String mod1;
 }
