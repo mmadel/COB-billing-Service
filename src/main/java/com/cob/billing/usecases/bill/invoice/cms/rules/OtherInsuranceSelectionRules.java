77835a003a6cb8766122467a1b27461d1694a29a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-02-21 16:15:21 +0200

    create component to select other patient insurance based on rules

---
+++ src/main/java/com/cob/billing/usecases/bill/invoice/cms/rules/OtherInsuranceSelectionRules.java
@@ -0,0 +1,12 @@
+package com.cob.billing.usecases.bill.invoice.cms.rules;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class OtherInsuranceSelectionRules {
+    public String[] select(List<String[]> otherInsurances ){
+        return null;
+    }
+}
