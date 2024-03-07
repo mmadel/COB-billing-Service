b1b38b2e8d7ac179530147f17ce331f82fae0bfd
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-07 14:47:33 +0200

    create patient auth model

---
+++ src/main/java/com/cob/billing/model/clinical/patient/auth/PatientAuthorization.java
@@ -0,0 +1,16 @@
+package com.cob.billing.model.clinical.patient.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PatientAuthorization {
+    private String authNumber;
+    private String[] insCompany;
+    private String serviceCode;
+    private Long startDateNumber;
+    private Long expireDateNumber;
+    private Integer visit;
+    private Integer remaining;
+}
