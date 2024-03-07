9f2b0936e6509edb400bb436dd91d408a7ebadb4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-07 15:21:06 +0200

    create skeleton for create patient auth use case and inject it into patient auth controller

--- src/main/java/com/cob/billing/controller/clinical/PatientAuthorizationController.java
+++ src/main/java/com/cob/billing/controller/clinical/PatientAuthorizationController.java
@@ -1,6 +1,10 @@
 package com.cob.billing.controller.clinical;
 
 import com.cob.billing.model.clinical.patient.auth.PatientAuthorization;
+import com.cob.billing.response.handler.ResponseHandler;
+import com.cob.billing.usecases.clinical.patient.auth.CreatePatientAuthorizationUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/authorization")
 public class PatientAuthorizationController {
 
+    @Autowired
+    CreatePatientAuthorizationUseCase createPatientAuthorizationUseCase;
+
     public ResponseEntity<Object> create(@RequestBody PatientAuthorization model) {
-        return null;
+        return ResponseHandler
+                .generateResponse("Successfully added Patient-Auth",
+                        HttpStatus.OK,
+                        createPatientAuthorizationUseCase.create(model));
     }
 }
---
+++ src/main/java/com/cob/billing/usecases/clinical/patient/auth/CreatePatientAuthorizationUseCase.java
@@ -0,0 +1,11 @@
+package com.cob.billing.usecases.clinical.patient.auth;
+
+import com.cob.billing.model.clinical.patient.auth.PatientAuthorization;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreatePatientAuthorizationUseCase {
+    public Long create(PatientAuthorization model){
+        return null;
+    }
+}
