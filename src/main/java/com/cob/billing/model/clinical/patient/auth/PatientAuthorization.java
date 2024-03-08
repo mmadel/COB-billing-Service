e85195877ed6cd953e7276e9133e79e052c0d93c
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-08 15:59:13 +0200

    add id field in patient authorization model

--- src/main/java/com/cob/billing/model/clinical/patient/auth/PatientAuthorization.java
+++ src/main/java/com/cob/billing/model/clinical/patient/auth/PatientAuthorization.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 @Setter
 @Getter
 public class PatientAuthorization {
+    private Long id;
     private String authNumber;
     private String[] insCompany;
     private String serviceCode;
