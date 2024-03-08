e69d5266e945aaa6f8b11ca06132551b8e2b738d
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-08 18:05:11 +0200

    create patient auth checker skeleton componenet

---
+++ src/main/java/com/cob/billing/usecases/clinical/patient/PatientAuthorizationCheckerUseCase.java
@@ -0,0 +1,10 @@
+package com.cob.billing.usecases.clinical.patient;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PatientAuthorizationCheckerUseCase {
+    public void check(){
+
+    }
+}
