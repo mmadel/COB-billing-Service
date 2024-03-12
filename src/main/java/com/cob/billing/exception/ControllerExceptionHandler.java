678fe54cf5687ffddd66610c1ca97097cfec630e
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-12 21:45:40 +0200

    handle billing exception in controller advice

--- src/main/java/com/cob/billing/exception/ControllerExceptionHandler.java
+++ src/main/java/com/cob/billing/exception/ControllerExceptionHandler.java
@@ -1,14 +1,41 @@
 package com.cob.billing.exception;
 
+import com.cob.billing.exception.business.BillingException;
+import com.cob.billing.exception.response.ControllerErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Locale;
 
 @ControllerAdvice
 @Slf4j
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     ResourceBundleMessageSource messageSource;
+
+    @ExceptionHandler(value = {BillingException.class})
+    public ResponseEntity handleFeedbackExceptionException(BillingException ex, WebRequest request) {
+        String errorMessage = messageSource.getMessage(ex.getCode(), ex.getParameters(), Locale.ENGLISH);
+        ControllerErrorResponse controllerErrorResponse = new ControllerErrorResponse(errorMessage, ex.getStatus() == null ? HttpStatus.INTERNAL_SERVER_ERROR : ex.getStatus());
+        log.error(controllerErrorResponse.getMessage());
+        return new ResponseEntity(controllerErrorResponse, ex.getStatus() == null ? HttpStatus.INTERNAL_SERVER_ERROR : ex.getStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        ControllerErrorResponse controllerErrorResponse = new ControllerErrorResponse(ex.getMessage(), status);
+        log.error(controllerErrorResponse.getMessage());
+        return new ResponseEntity<>(controllerErrorResponse, status);
+    }
 }
