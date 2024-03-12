917ead9b728c01eb9df7dda477946c5c57a22ed4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-12 21:30:54 +0200

    add ResourceBundleMessageSource bean

--- src/main/java/com/cob/billing/configuration/ApplicationConfiguration.java
+++ src/main/java/com/cob/billing/configuration/ApplicationConfiguration.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -16,5 +17,15 @@ public class ApplicationConfiguration {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasenames("exception");
+        source.setDefaultEncoding("UTF-8");
+        source.setAlwaysUseMessageFormat(true);
+
+        return source;
     }
 }
