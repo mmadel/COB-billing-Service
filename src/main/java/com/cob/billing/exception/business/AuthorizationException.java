7ca9f8a8904b3e128ae9999839394bc37f5cad3c
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-12 21:40:02 +0200

    create authorization exception class , change path of exception message file to be under message folder

---
+++ src/main/java/com/cob/billing/exception/business/AuthorizationException.java
@@ -0,0 +1,28 @@
+package com.cob.billing.exception.business;
+
+import org.springframework.http.HttpStatus;
+
+public class AuthorizationException extends BillingException {
+    public static final String AUTH_MULTIPLE_SESSIONS = Category.Business.value() + getPrefix() + "_00";
+    public static final String AUTH_OVERLAP = Category.Business.value() + getPrefix() + "_01";
+    public static final String AUTH_EXPIRATION = Category.Business.value() + getPrefix() + "_02";
+    public static final String AUTH_CREDIT_REMAINING = Category.Business.value() + getPrefix() + "_03";
+
+    public AuthorizationException(String code) {
+        super(code);
+    }
+
+    public AuthorizationException(String code, Object[] parameters) {
+        super(code, parameters);
+    }
+
+    public AuthorizationException(HttpStatus status, String code, Object[] parameters) {
+        super(status, code, parameters);
+    }
+
+    protected static String getPrefix() {
+        return "_auth";
+    }
+
+}
+
