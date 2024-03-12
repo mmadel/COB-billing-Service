47eacb8eab798b6f7b7843dd2e7812edbea69ffe
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-12 21:32:57 +0200

    create ControllerExceptionHandler component

---
+++ src/main/java/com/cob/billing/exception/ControllerExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.cob.billing.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    @Autowired
+    ResourceBundleMessageSource messageSource;
+}
