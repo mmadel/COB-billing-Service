30527219ef59b18e6c1d2b4247b52d118afcadb4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-17 20:14:21 +0200

    check if session is attached to auth

--- src/main/java/com/cob/billing/usecases/clinical/patient/auth/watching/AuthorizationSessionSubmission.java
+++ src/main/java/com/cob/billing/usecases/clinical/patient/auth/watching/AuthorizationSessionSubmission.java
@@ -1,6 +1,6 @@
 package com.cob.billing.usecases.clinical.patient.auth.watching;
 
-import com.cob.billing.model.bill.invoice.tmp.InvoiceRequest;
+import com.cob.billing.entity.clinical.patient.session.PatientSessionEntity;
 import com.cob.billing.model.clinical.patient.session.PatientSession;
 import com.cob.billing.repositories.clinical.session.PatientSessionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +11,11 @@ public class AuthorizationSessionSubmission {
     @Autowired
     PatientSessionRepository patientSessionRepository;
     public void submit(PatientSession patientSession){
-
+        PatientSessionEntity session =  patientSessionRepository.findById(patientSession.getId()).get();
+        if(session.getPatientAuthorization() != null){
+            System.out.println("session attached, call validation COR");
+        }else{
+            System.out.println("session not attached, call selection COR");
+        }
     }
 }
