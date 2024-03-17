d1ac61df5dcf1dab30c87517e88c423bf2fd1fc6
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-17 23:17:57 +0200

    impl session auth has no credit validation

--- src/main/java/com/cob/billing/exception/business/AuthorizationException.java
+++ src/main/java/com/cob/billing/exception/business/AuthorizationException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class AuthorizationException extends BillingException {
     public static final String SESSION_AUTH_OUT_OF_RANGE = Category.Business.value() + getPrefix() + "_00";
-    public static final String AUTH_EXPIRATION = Category.Business.value() + getPrefix() + "_01";
+    public static final String SESSION_AUTH_NO_REMAINING = Category.Business.value() + getPrefix() + "_01";
     public static final String AUTH_CREDIT_REMAINING = Category.Business.value() + getPrefix() + "_02";
 
     public AuthorizationException(String code) {
--- src/main/java/com/cob/billing/usecases/clinical/patient/auth/watching/validation/SessionNoCreditAuthorizationValidation.java
+++ src/main/java/com/cob/billing/usecases/clinical/patient/auth/watching/validation/SessionNoCreditAuthorizationValidation.java
@@ -2,18 +2,25 @@ package com.cob.billing.usecases.clinical.patient.auth.watching.validation;
 
 import com.cob.billing.exception.business.AuthorizationException;
 import com.cob.billing.model.clinical.patient.session.PatientSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SessionNoCreditAuthorizationValidation implements SessionAuthorizationValidation {
     private SessionAuthorizationValidation sessionAuthorizationValidation;
+
     @Override
     public void setNextValidation(SessionAuthorizationValidation nextValidation) {
         this.sessionAuthorizationValidation = nextValidation;
     }
 
     @Override
-    public void processRequest(PatientSession patientSession,Long[] authorizationData) throws AuthorizationException {
+    public void processRequest(PatientSession patientSession, Long[] authorizationData) throws AuthorizationException {
+        if (authorizationData[2] == 0)
+            throw new AuthorizationException(HttpStatus.CONFLICT, AuthorizationException.SESSION_AUTH_NO_REMAINING, new Object[]{patientSession.getServiceDate().toString()});
+        else
+            sessionAuthorizationValidation.processRequest(patientSession, authorizationData);
+
 
     }
 }
