0245d395535f6176f33df2128af3c5c704f94049
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-18 19:39:04 +0200

    create component to select authorization for not assigned session

---
+++ src/main/java/com/cob/billing/usecases/clinical/patient/auth/watching/selection/SelectAuthorizationForNotAssignedSessionUseCase.java
@@ -0,0 +1,10 @@
+package com.cob.billing.usecases.clinical.patient.auth.watching.selection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SelectAuthorizationForNotAssignedSessionUseCase {
+    public void select(){
+
+    }
+}
