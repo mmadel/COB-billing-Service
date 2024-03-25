8146fd3fed4683e382c74cfd179fbc3538d5abf9
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-25 21:21:14 +0200

    impl method to construct session history response in service to be returned

--- src/main/java/com/cob/billing/usecases/bill/history/SearchSessionHistoryUseCase.java
+++ src/main/java/com/cob/billing/usecases/bill/history/SearchSessionHistoryUseCase.java
@@ -1,11 +1,25 @@
 package com.cob.billing.usecases.bill.history;
 
 import com.cob.billing.model.bill.invoice.search.SessionHistoryCriteria;
+import com.cob.billing.model.clinical.patient.session.PatientSessionServiceLine;
+import com.cob.billing.model.response.SessionHistoryResponse;
+import com.cob.billing.util.PaginationUtil;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SearchSessionHistoryUseCase {
-    public void search(SessionHistoryCriteria sessionHistoryCriteria){
+    public void search(SessionHistoryCriteria sessionHistoryCriteria) {
 
+    }
+
+    private SessionHistoryResponse constructSessionHistoryResponse(int offset, int limit) {
+        List<PatientSessionServiceLine> records = PaginationUtil.paginate(null, offset, limit);
+        return SessionHistoryResponse.builder()
+                .number_of_records(null) //patientSessionServiceLines.size()
+                .number_of_matching_records(records.size())
+                .records(null) //records
+                .build();
     }
 }
