eddd1d6a7ef2d4b18564ef79511added26ab8b7e
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-25 21:13:06 +0200

    add session history search service

---
+++ src/main/java/com/cob/billing/usecases/bill/history/SearchSessionHistoryUseCase.java
@@ -0,0 +1,11 @@
+package com.cob.billing.usecases.bill.history;
+
+import com.cob.billing.model.bill.invoice.search.SessionHistoryCriteria;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchSessionHistoryUseCase {
+    public void search(SessionHistoryCriteria sessionHistoryCriteria){
+
+    }
+}
