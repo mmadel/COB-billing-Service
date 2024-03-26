91f7ffd96e87da0d552b7ac86bd7d0dea94f6bff
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-26 21:43:45 +0200

    create controller to expose delete by id fee schedule service

--- src/main/java/com/cob/billing/controller/bill/FeeScheduleController.java
+++ src/main/java/com/cob/billing/controller/bill/FeeScheduleController.java
@@ -3,6 +3,7 @@ package com.cob.billing.controller.bill;
 import com.cob.billing.model.bill.fee.schedule.FeeScheduleModel;
 import com.cob.billing.usecases.bill.tools.fee.schedule.CreateFeeScheduleUseCase;
 import com.cob.billing.usecases.bill.tools.fee.schedule.FindFeeSchedulesUseCase;
+import com.cob.billing.usecases.bill.tools.fee.schedule.RemoveFeeSchedulesUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,9 @@ public class FeeScheduleController {
     @Autowired
     FindFeeSchedulesUseCase findFeeSchedulesUseCase;
 
+    @Autowired
+    RemoveFeeSchedulesUseCase removeFeeSchedulesUseCase;
+
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody FeeScheduleModel model) {
         return new ResponseEntity(createFeeScheduleUseCase.createFeeSchedule(model), HttpStatus.OK);
@@ -29,5 +33,9 @@ public class FeeScheduleController {
     @GetMapping("/find/id/{id}")
     public ResponseEntity find(@PathVariable Long id) {
         return new ResponseEntity(findFeeSchedulesUseCase.findById(id), HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        return new ResponseEntity(removeFeeSchedulesUseCase.remove(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/cob/billing/repositories/bill/fee/schedule/FeeScheduleLineRepository.java
+++ src/main/java/com/cob/billing/repositories/bill/fee/schedule/FeeScheduleLineRepository.java
@@ -3,5 +3,10 @@ package com.cob.billing.repositories.bill.fee.schedule;
 import com.cob.billing.entity.bill.fee.schedule.FeeScheduleLineEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FeeScheduleLineRepository  extends CrudRepository<FeeScheduleLineEntity,Long> {
+
+    Optional<List<FeeScheduleLineEntity>> findByFeeSchedule_Id(Long id);
 }
