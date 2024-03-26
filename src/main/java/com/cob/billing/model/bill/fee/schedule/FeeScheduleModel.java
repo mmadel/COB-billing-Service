d00c3cf733b42745d41a6fa24b0e1434862a3304
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-26 21:02:45 +0200

    create fee schedule service and models

---
+++ src/main/java/com/cob/billing/model/bill/fee/schedule/FeeScheduleModel.java
@@ -0,0 +1,14 @@
+package com.cob.billing.model.bill.fee.schedule;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class FeeScheduleModel {
+    private Long id;
+    private Boolean defaultFee;
+    private List<FeeScheduleLineModel> feeLines;
+}
---
+++ src/main/java/com/cob/billing/usecases/bill/tools/fee/schedule/CreateFeeScheduleUseCase.java
@@ -0,0 +1,9 @@
+package com.cob.billing.usecases.bill.tools.fee.schedule;
+
+import com.cob.billing.model.bill.fee.schedule.FeeScheduleModel;
+
+public class CreateFeeScheduleUseCase {
+    public void createFeeSchedule(FeeScheduleModel model) {
+
+    }
+}
