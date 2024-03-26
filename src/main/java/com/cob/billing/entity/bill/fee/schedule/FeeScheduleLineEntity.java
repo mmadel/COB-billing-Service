ab459610f3969d3ffcde9c36975c2356f6d436b1
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-26 20:55:11 +0200

    create fee schedule and it's lines entities

---
+++ src/main/java/com/cob/billing/entity/bill/fee/schedule/FeeScheduleLineEntity.java
@@ -0,0 +1,32 @@
+package com.cob.billing.entity.bill.fee.schedule;
+
+import com.cob.billing.enums.RateType;
+import com.cob.billing.enums.SubmissionStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fee_schedule_line")
+@Setter
+@Getter
+public class FeeScheduleLineEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "cpt_code")
+    private String cptCode;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rate_type")
+    private RateType rateType;
+    @Column(name = "per_unit")
+    private Integer perUnit;
+    @Column(name = "charge_amount")
+    private Float chargeAmount;
+    @ManyToOne
+    @JoinColumn(name="fee_schedule_id", nullable=false)
+    private FeeScheduleEntity feeSchedule;
+
+}
