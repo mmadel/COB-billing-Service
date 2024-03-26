d24bccd319d63e0d688ca47a377d738ed3ddbe36
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-26 21:34:26 +0200

    create controller to expose find fee schedule service

--- src/main/java/com/cob/billing/controller/bill/FeeScheduleController.java
+++ src/main/java/com/cob/billing/controller/bill/FeeScheduleController.java
@@ -2,13 +2,11 @@ package com.cob.billing.controller.bill;
 
 import com.cob.billing.model.bill.fee.schedule.FeeScheduleModel;
 import com.cob.billing.usecases.bill.tools.fee.schedule.CreateFeeScheduleUseCase;
+import com.cob.billing.usecases.bill.tools.fee.schedule.FindFeeSchedulesUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/fee/schedule")
@@ -16,8 +14,16 @@ public class FeeScheduleController {
     @Autowired
     CreateFeeScheduleUseCase createFeeScheduleUseCase;
 
+    @Autowired
+    FindFeeSchedulesUseCase findFeeSchedulesUseCase;
+
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody FeeScheduleModel model) {
         return new ResponseEntity(createFeeScheduleUseCase.createFeeSchedule(model), HttpStatus.OK);
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity find() {
+        return new ResponseEntity(findFeeSchedulesUseCase.find(), HttpStatus.OK);
     }
 }
