48a69d708d500463a6ba4e4a8eaa40943711c67b
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-26 21:04:56 +0200

    impl  fee schedule service to create it

--- src/main/java/com/cob/billing/usecases/bill/tools/fee/schedule/CreateFeeScheduleUseCase.java
+++ src/main/java/com/cob/billing/usecases/bill/tools/fee/schedule/CreateFeeScheduleUseCase.java
@@ -1,9 +1,21 @@
 package com.cob.billing.usecases.bill.tools.fee.schedule;
 
+import com.cob.billing.entity.bill.fee.schedule.FeeScheduleEntity;
 import com.cob.billing.model.bill.fee.schedule.FeeScheduleModel;
+import com.cob.billing.repositories.bill.fee.schedule.FeeScheduleRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CreateFeeScheduleUseCase {
-    public void createFeeSchedule(FeeScheduleModel model) {
-
+    @Autowired
+    FeeScheduleRepository feeScheduleRepository;
+    @Autowired
+    ModelMapper mapper;
+    public Long createFeeSchedule(FeeScheduleModel model) {
+        FeeScheduleEntity feeSchedule = mapper.map(model , FeeScheduleEntity.class);
+        feeScheduleRepository.save(feeSchedule);
+        return feeSchedule.getId();
     }
 }
