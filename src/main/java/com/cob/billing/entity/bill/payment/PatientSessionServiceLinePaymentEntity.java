dc0dbc266bd39b0f5a1447c71ef37883f8853cb8
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-31 21:22:23 +0200

    add service Line payment entity and its type and action enums

---
+++ src/main/java/com/cob/billing/entity/bill/payment/PatientSessionServiceLinePaymentEntity.java
@@ -0,0 +1,42 @@
+package com.cob.billing.entity.bill.payment;
+
+import com.cob.billing.enums.ServiceLinePaymentAction;
+import com.cob.billing.enums.ServiceLinePaymentType;
+import com.cob.billing.model.bill.posting.PaymentBatch;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "patient_session_service_line_payment")
+@Setter
+@Getter
+public class PatientSessionServiceLinePaymentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "balance")
+    private double balance;
+
+    @Column(name = "payment")
+    private double payment;
+
+    @Column(name = "adjust")
+    private double adjust;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_action")
+    private ServiceLinePaymentAction serviceLinePaymentAction;
+
+    @Column(name = "payment_batch", columnDefinition = "json")
+    @Type(type = "json")
+    private PaymentBatch paymentBatch;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_type")
+    private ServiceLinePaymentType serviceLinePaymentType;
+}
