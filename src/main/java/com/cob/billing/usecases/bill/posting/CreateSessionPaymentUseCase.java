d590b14c8f51bfafc3f43b684315dca2dcc19f7b
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-03-31 21:10:32 +0200

    add skeleton for create session payment use case

---
+++ src/main/java/com/cob/billing/usecases/bill/posting/CreateSessionPaymentUseCase.java
@@ -0,0 +1,10 @@
+package com.cob.billing.usecases.bill.posting;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateSessionPaymentUseCase {
+    public void create(){
+
+    }
+}
