d6882d0b56463ba4d4000decbf913ec61f9fd64f
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-08 02:49:50 +0200

    add method to patientSessionRepository to find ,client pending  sessions

--- src/main/java/com/cob/billing/controller/bill/ClientBalanceController.java
+++ src/main/java/com/cob/billing/controller/bill/ClientBalanceController.java
@@ -1,22 +1,26 @@
 package com.cob.billing.controller.bill;
 
+import com.cob.billing.model.bill.posting.balance.FindClientPendingServiceLinesUseCase;
+import com.cob.billing.model.clinical.patient.session.filter.PatientSessionSearchCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/client/balance")
 public class ClientBalanceController {
-
-    @GetMapping("/find/awaiting/{serviceLinesIds}")
-    public ResponseEntity findAwaiting(@PathVariable Long[] serviceLinesIds) {
-        return new ResponseEntity(null, HttpStatus.OK);
+    @Autowired
+    FindClientPendingServiceLinesUseCase findClientPendingServiceLinesUseCase;
+    @PostMapping("/find/awaiting/patient/{patientId}")
+    public ResponseEntity findAwaiting(@PathVariable(name = "patientId") Long patientId
+            , @RequestBody PatientSessionSearchCriteria patientSessionSearchCriteria) {
+        return new ResponseEntity(findClientPendingServiceLinesUseCase.find(patientId,patientSessionSearchCriteria), HttpStatus.OK);
     }
-    @GetMapping("/find/Finalized/{serviceLinesIds}")
-    public ResponseEntity findFinalized(@PathVariable Long[] serviceLinesIds) {
+
+    @PostMapping("/find/Finalized/patient/{patientId}")
+    public ResponseEntity findFinalized(@PathVariable(name = "patientId") Long patientId
+            , @RequestBody PatientSessionSearchCriteria patientSessionSearchCriteria) {
         return new ResponseEntity(null, HttpStatus.OK);
     }
 }
