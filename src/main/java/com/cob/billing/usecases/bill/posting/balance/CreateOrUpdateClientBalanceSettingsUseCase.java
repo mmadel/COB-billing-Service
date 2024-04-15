4910e1fe0725a2682868f6a5c4fd92c7d1a70cc4
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-15 16:02:57 +0200

    create skeleton for create update and Retrieve client balance settings use cases

---
+++ src/main/java/com/cob/billing/usecases/bill/posting/balance/CreateOrUpdateClientBalanceSettingsUseCase.java
@@ -0,0 +1,11 @@
+package com.cob.billing.usecases.bill.posting.balance;
+
+import com.cob.billing.entity.bill.balance.PatientBalanceAccountSettings;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateOrUpdateClientBalanceSettingsUseCase {
+    public void create(PatientBalanceAccountSettings model){
+
+    }
+}
