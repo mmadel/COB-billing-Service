76ccb3ba1f2e4b48496be0efecf13fbeff715606
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-25 14:27:34 +0200

    impl export client receipt

--- src/main/java/com/cob/billing/controller/bill/BatchPaymentController.java
+++ src/main/java/com/cob/billing/controller/bill/BatchPaymentController.java
@@ -2,6 +2,7 @@ package com.cob.billing.controller.bill;
 
 import com.cob.billing.model.bill.posting.paymnet.ServiceLinePaymentRequest;
 import com.cob.billing.usecases.bill.posting.CreateSessionServiceLinePaymentUseCase;
+import com.cob.billing.usecases.bill.posting.GenerateClientBatchReceiptPDFUseCase;
 import com.cob.billing.usecases.bill.posting.batching.CreateBatchClientPaymentUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,13 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
 @RestController
-@RequestMapping(value = "/batch/paymentm")
+@RequestMapping(value = "/batch/payment")
 public class BatchPaymentController {
     @Autowired
     CreateSessionServiceLinePaymentUseCase createSessionServiceLinePaymentUseCase;
     @Autowired
     CreateBatchClientPaymentUseCase createBatchClientPaymentUseCase;
+    @Autowired
+    GenerateClientBatchReceiptPDFUseCase generateClientBatchReceiptPDFUseCase;
 
     @PostMapping("/client/create")
     public ResponseEntity create(@RequestBody ServiceLinePaymentRequest model) {
@@ -29,5 +36,18 @@ public class BatchPaymentController {
     public ResponseEntity createInsuranceCompany(@RequestBody ServiceLinePaymentRequest model) {
         createSessionServiceLinePaymentUseCase.create(model);
         return new ResponseEntity(HttpStatus.OK);
+    }
+    @PostMapping("/client/receipt/pdf")
+    public void exportPdf( HttpServletResponse response) throws IOException {
+        response.setHeader("Expires", "0");
+        response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+        response.setHeader("Pragma", "public");
+        response.setContentType("application/pdf");
+        byte[] pdfAsBytes = generateClientBatchReceiptPDFUseCase.create();
+        response.setContentLength(pdfAsBytes.length);
+        OutputStream os = response.getOutputStream();
+        os.write(pdfAsBytes);
+        os.flush();
+        os.close();
     }
 }
