e0dd050aba735a5a31c70d98ed474dfc64cae2ee
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-04-25 19:53:46 +0200

    return session as Distinct

--- src/main/java/com/cob/billing/usecases/bill/posting/batching/CreateBatchClientPaymentUseCase.java
+++ src/main/java/com/cob/billing/usecases/bill/posting/batching/CreateBatchClientPaymentUseCase.java
@@ -2,8 +2,10 @@ package com.cob.billing.usecases.bill.posting.batching;
 
 import com.cob.billing.entity.clinical.patient.PatientEntity;
 import com.cob.billing.entity.clinical.patient.session.PatientSessionEntity;
+import com.cob.billing.model.admin.clinic.Clinic;
 import com.cob.billing.model.bill.posting.paymnet.ServiceLinePaymentRequest;
 import com.cob.billing.model.bill.posting.paymnet.SessionServiceLinePayment;
+import com.cob.billing.model.bill.posting.paymnet.batch.pdf.ClientBatchReceiptLocationInfo;
 import com.cob.billing.model.bill.posting.paymnet.batch.pdf.ClientBatchReceiptPatientInfo;
 import com.cob.billing.model.bill.posting.paymnet.batch.pdf.ClientBatchReceiptPaymentInfo;
 import com.cob.billing.model.bill.posting.paymnet.batch.pdf.ClientBatchReceiptRequest;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -39,12 +42,15 @@ public class CreateBatchClientPaymentUseCase {
                 .stream(patientSessionRepository.findAllById(paymentsGroupedBySession.keySet()).spliterator(), false)
                 .collect(Collectors.toList());
 
+
         fillClientInfo(sessions.stream().findFirst().get().getPatient(), model);
 
         fillPaymentInfo(serviceLinePaymentRequest.getReceivedDate(),
                 serviceLinePaymentRequest.getPaymentMethod(),
                 serviceLinePaymentRequest.getTotalAmount(),
                 model);
+
+        fillLocation(sessions, model);
         return model;
     }
 
@@ -65,5 +71,29 @@ public class CreateBatchClientPaymentUseCase {
         clientBatchReceiptPaymentInfo.setPaymentMethod(paymentMethod);
         clientBatchReceiptPaymentInfo.setTotalPayment(totalAmount.floatValue());
         model.setClientBatchReceiptPaymentInfo(clientBatchReceiptPaymentInfo);
+    }
+
+    private void fillLocation(List<PatientSessionEntity> sessions, ClientBatchReceiptRequest model) {
+        List<ClientBatchReceiptLocationInfo> clientBatchReceiptLocationInfo = new ArrayList<>();
+        List<PatientSessionEntity> sessionsDistinct = sessions.stream()
+                .collect(Collectors.toMap(
+                        session -> session.getClinic().getId(),  // Key is the clinic id
+                        session -> session,                     // Value is the session entity
+                        (existing, replacement) -> existing     // If duplicates, keep the existing session entity
+                ))
+                .values()
+                .stream()
+                .collect(Collectors.toList());
+        for (PatientSessionEntity session : sessionsDistinct) {
+            ClientBatchReceiptLocationInfo locationInfo = new ClientBatchReceiptLocationInfo();
+            locationInfo.setLocationName(session.getClinic().getTitle());
+            String locationAddress = session.getClinic().getClinicdata().getAddress()
+                    + "," + session.getClinic().getClinicdata().getCity()
+                    + "," + session.getClinic().getClinicdata().getState()
+                    + "," + session.getClinic().getClinicdata().getZipCode();
+            locationInfo.setLocationAddress(locationAddress);
+            clientBatchReceiptLocationInfo.add(locationInfo);
+        }
+        model.setClientBatchReceiptLocationInfo(clientBatchReceiptLocationInfo);
     }
 }
