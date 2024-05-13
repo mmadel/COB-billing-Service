d42f7b3c21f973d527c9c01da5ebdbe95a4fd90b
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-13 09:04:32 +0300

    add permission to patient controllers

--- src/main/java/com/cob/billing/controller/clinical/ProviderController.java
+++ src/main/java/com/cob/billing/controller/clinical/ProviderController.java
@@ -9,10 +9,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/provider")
+@PreAuthorize("hasAnyRole('soild-provider-role')")
 public class ProviderController {
     @Autowired
     CreateProviderUseCase createProviderUseCase;
