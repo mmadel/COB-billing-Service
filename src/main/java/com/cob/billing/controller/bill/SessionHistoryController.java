3f3268075092aba4d2ee0650cf768b894f3522dd
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-13 09:07:55 +0300

    add permission to session history controller

--- src/main/java/com/cob/billing/controller/bill/SessionHistoryController.java
+++ src/main/java/com/cob/billing/controller/bill/SessionHistoryController.java
@@ -11,10 +11,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/session/history")
+@PreAuthorize("hasAnyRole('filing-role')")
 public class SessionHistoryController {
     @Autowired
     FindSessionHistoryUseCase findSessionHistoryUseCase;
