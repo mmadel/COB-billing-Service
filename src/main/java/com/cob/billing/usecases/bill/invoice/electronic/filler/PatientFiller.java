98c93c839e09dc46d84fe07dd6afc27b418d8968
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-08-13 10:23:09 +0300

    correct set pcn in electronic claim to be set based on box 26 configuration

--- src/main/java/com/cob/billing/usecases/bill/invoice/electronic/filler/PatientFiller.java
+++ src/main/java/com/cob/billing/usecases/bill/invoice/electronic/filler/PatientFiller.java
@@ -16,14 +16,24 @@ public class PatientFiller {
         claim.setPat_zip(patientInformation.getAddress().getZipCode());
         SimpleDateFormat dobFormat = new SimpleDateFormat("yyyy/MM/dd");
         claim.setPat_dob(dobFormat.format(patientInformation.getDateOfBirth()));
-        if (patientInformation.getPatientAdvancedInformation()!= null){
+        if (patientInformation.getPatientAdvancedInformation() != null) {
             claim.setAccident_date(dobFormat.format(patientInformation.getPatientAdvancedInformation().getPatientAdvancedDates().getAccident()));
             claim.setAuto_accident(patientInformation.getPatientAdvancedInformation().getPateintAdvancedCondtion().isAutoAccident() ? "Y" : "N");
             claim.setEmployment_related(patientInformation.getPatientAdvancedInformation().getPateintAdvancedCondtion().isEmployment() ? "Y" : "N");
         }
         claim.setPat_name_f(patientInformation.getFirstName());
         claim.setPat_name_l(patientInformation.getLastName());
-        claim.setPcn(patientInformation.getSsn());
+        switch (patientInformation.getBox26()) {
+            case "primaryId":
+                claim.setPcn(patientInformation.getInsuredPrimaryId());
+                break;
+            case "ssn":
+                claim.setPcn(patientInformation.getSsn());
+                break;
+            case "externalId":
+                claim.setPcn(patientInformation.getExternalId());
+                break;
+        }
         claim.setPat_sex(patientInformation.getGender().toString().charAt(0) + "");
     }
 }
