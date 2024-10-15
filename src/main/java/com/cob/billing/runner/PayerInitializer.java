086eab36e81835bd1142318c60646bdf50409af5
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-10-15 19:42:23 +0300

    add logs to PayerInitializer

--- src/main/java/com/cob/billing/runner/PayerInitializer.java
+++ src/main/java/com/cob/billing/runner/PayerInitializer.java
@@ -2,22 +2,24 @@ package com.cob.billing.runner;
 
 import com.cob.billing.repositories.bill.payer.PayerRepository;
 import com.cob.billing.usecases.bill.payer.AddPayerUseCase;
-import org.checkerframework.checker.units.qual.A;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class PayerInitializer implements CommandLineRunner {
     @Autowired
     PayerRepository payerRepository;
     @Autowired
     AddPayerUseCase addPayerUseCase;
+
     @Override
     public void run(String... args) {
         boolean hasPayers = payerRepository.existsBy();
-        if(!hasPayers){
-            System.out.println("Add Payers...");
+        if (!hasPayers) {
+            log.info("Payers have been added to cache.");
             addPayerUseCase.add();
         }
     }
